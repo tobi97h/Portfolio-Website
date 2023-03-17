4d20aabd3ce68eae8b36e9fae8354e6ad5c2f7ec
s.claes <sebastian.claes@protonmail.com>
2023-03-17 11:59:10 +0100

    [ADD] margin adjustments

--- src/components/home-view/RebalancingTool.vue
+++ src/components/home-view/RebalancingTool.vue
@@ -6,8 +6,7 @@
   <section id="project-section">
     <div class="container">
 
-      <p class="subheading">Aktuelle Projekte</p>
-      <h2>Portfolio Rebalancing Tool</h2>
+      <p class="subheading">Eigne Projekte</p>
       <div class="divider"></div>
 
       <div class="project-wrapper art">
@@ -15,8 +14,8 @@
           <h3>Portfolio Rebalancing Tool</h3>
           <p class="text">Mit unserer App können Sie Ihre Vermögenswerte einfach und vollumfänglich abbilden. Geplant ist eine automatisierte Wiederherstellung ihrer Vermögensverteilung anhand einer selbst gewählten Mit unserer App können Sie Ihre Vermögenswerte einfach und vollumfänglich abbilden. Geplant ist eine automatisierte Wiederherstellung ihrer Vermögensverteilung anhand einer selbst gewählten Strategie. Mit unserer App können Sie Ihre Vermögenswerte einfach und vollumfänglich abbilden. Geplant ist eine automatisierte Wiederherstellung ihrer Vermögensverteilung anhand einer selbst gewählten Strategie.</p>
           <div class="button-wrapper">
-            <a href="#" class="button">Zur Live Version</a>
-            <a href="#" class="button">Zum Quellcode</a>
+            <a href="https://portfolio-rebalancer.com/" target="_blank" class="button">Zur Live Version</a>
+            <a href="https://github.com/Asset-Rebalancing-Tool/ApiRunner" target="_blank" class="button">Zum Quellcode</a>
           </div>
         </div>
         <img class="art-wireframes" src="src/assets/images/Portfolio_Rebalancing_Tool_Wireframes.png" alt="Portfolio Rebalancing Tool Wireframes" />
@@ -27,8 +26,8 @@
           <h3>Suggest App</h3>
           <p class="text">Finde Menschen mit einem nahezu identischem Musikgeschmack und entdecke die Lieblingsongs deiner Seelenverwanden, die du noch nicht kennst. Finde Menschen mit einem nahezu identischem Musikgeschmack und entdecke die Lieblingsongs deiner Seelenverwanden, die du noch nicht kennst. Finde Menschen mit einem nahezu identischem Musikgeschmack und entdecke die Lieblingsongs deiner Seelenverwanden, die du noch nicht kennst. die du noch nicht kennst. Finde Menschen mit einem nahezu identischem Musikgeschmack und entdecke die Lieblingsongs deiner Seelenverwanden, die du noch nicht kennst.</p>
           <div class="button-wrapper">
-            <a href="#" class="button">Zur Live Version</a>
-            <a href="#" class="button">Zum Quellcode</a>
+            <a href="https://suggest-app.com/" target="_blank" class="button">Zur Live Version</a>
+            <a href="https://github.com/Suggest-App/SGBackend" target="_blank" class="button">Zum Quellcode</a>
           </div>
         </div>
         <img class="sg-wireframes" src="src/assets/images/Suggest_App_Wireframes.png" alt="Suggest App Wireframes" />
