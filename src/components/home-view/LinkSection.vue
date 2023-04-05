31b57c44263795fbc47f075adbf72df0b4d51afa
tobi97h <tobi_h@outlook.com>
2023-04-05 15:21:54 +0200

    update texts

--- src/components/home-view/LinkSection.vue
+++ src/components/home-view/LinkSection.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import axios from "axios"
-import { ref, onMounted } from "vue";
+import {ref, onMounted, reactive} from "vue";
 import type { Ref } from "vue";
 import type { Stats } from "@/models/stats/Stats";
 import { useI18n } from "vue-i18n";
@@ -8,17 +8,11 @@ import statsApi from "@/services/StatsApi";
 
 const { t } = useI18n()
 
-let stats: Stats = {
-  ghostBlogEntries: 404,
-  linesOfCode: 404,
-  commits: 404,
-  repos: 404
-} as Stats
+let stats = reactive({} as Stats)
 
 onMounted(async () => {
   stats = await statsApi.fetchStats()
 })
-
 
 </script>
 
@@ -35,29 +29,29 @@ onMounted(async () => {
           <div class="arrow"></div>
         </a>
 
-        <div class="link-box git">
+        <a class="link-box git">
           <h4>{{ stats.repos}}</h4>
           <div class="divider"></div>
           <h2>Git Repos</h2>
           <p>{{ $t("stats.repos") }}</p>
           <div class="arrow"></div>
-        </div>
+        </a>
 
-        <div class="link-box git">
+        <a class="link-box git">
           <h4>{{ stats.commits }}</h4>
           <div class="divider"></div>
           <h2>Git Commits</h2>
           <p>{{ $t("stats.commits") }}</p>
           <div class="arrow"></div>
-        </div>
+        </a>
 
-        <div class="link-box git">
+        <a class="link-box git">
           <h4>{{ stats.linesOfCode }}</h4>
           <div class="divider"></div>
           <h2>{{ $t("stats.linesOfCode.heading")}}</h2>
           <p>{{ $t("stats.linesOfCode.text") }}</p>
           <div class="arrow"></div>
-        </div>
+        </a>
 
 
       </div>
